22783e273aa5b4fb82ea2a22ebae4e9444dc7b2d
hsiaoairplane <hsiaoairplane@gmail.com>
2018-05-06 00:02:15 +0800

    Add data cache test cases.
    Signed-off-by: hsiaoairplane <hsiaoairplane@gmail.com>

--- backend/cache/data.go
+++ backend/cache/data.go
@@ -1,6 +1,8 @@
 package cache
 
-import "sort"
+import (
+	"sort"
+)
 
 // topic defines the topic voting information (upvote/downvote)
 type vote struct {
@@ -10,61 +12,10 @@ type vote struct {
 
 // Keeps the topics in-memory data cache
 // Key: topic name ; Value: vote
-var topicKV map[string]vote
+var topicKV map[string]*vote
 
 func init() {
-	topicKV = make(map[string]vote)
-}
-
-// Sorting via criteria upvote
-type topic struct {
-	name     string
-	upvote   uint64
-	downvote uint64
-}
-
-// TopicListUpvote defines the topic array with upvote
-type TopicListUpvote []topic
-
-func (l TopicListUpvote) Len() int           { return len(l) }
-func (l TopicListUpvote) Swap(i, j int)      { l[i], l[j] = l[j], l[i] }
-func (l TopicListUpvote) Less(i, j int) bool { return l[i].upvote < l[j].upvote }
-
-// TopicListDownvote defines the topic array with downvote
-type TopicListDownvote []topic
-
-func (l TopicListDownvote) Len() int           { return len(l) }
-func (l TopicListDownvote) Swap(i, j int)      { l[i], l[j] = l[j], l[i] }
-func (l TopicListDownvote) Less(i, j int) bool { return l[i].downvote < l[j].downvote }
-
-// GetTopicDescendUpvote gets topics with desceding upvote order
-func GetTopicDescendUpvote() TopicListUpvote {
-	// Transfer map to array
-	uvList := make(TopicListUpvote, len(topicKV))
-	// Variable default value is 0
-	var index int
-	for k, v := range topicKV {
-		uvList[index] = topic{name: k, upvote: v.upvote, downvote: v.downvote}
-		index++
-	}
-
-	sort.Sort(TopicListUpvote(uvList))
-	return uvList
-}
-
-// GetTopicDescendDownvote gets topics with desceding downvote order
-func GetTopicDescendDownvote() TopicListDownvote {
-	// Transfer map to array
-	dvList := make(TopicListDownvote, len(topicKV))
-	// Variable default value is 0
-	var index int
-	for k, v := range topicKV {
-		dvList[index] = topic{name: k, upvote: v.upvote, downvote: v.downvote}
-		index++
-	}
-
-	sort.Sort(TopicListDownvote(dvList))
-	return dvList
+	topicKV = make(map[string]*vote)
 }
 
 // IsTopicExist checks is the topic name exists in in-memory data cache
@@ -80,7 +31,7 @@ func CreateTopic(name string) bool {
 		return true
 	}
 
-	topicKV[name] = vote{}
+	topicKV[name] = &vote{}
 	return true
 }
 
@@ -129,42 +80,53 @@ func SetTopicDownvote(name string, count uint64) bool {
 	return false
 }
 
-// IncTopicUpvote increases topic upvote counts
-func IncTopicUpvote(name string) bool {
-	if v, ok := topicKV[name]; ok {
-		v.upvote = v.upvote + 1
-		return true
-	}
-	return false
+// Sorting via criteria upvote
+type topic struct {
+	name     string
+	upvote   uint64
+	downvote uint64
 }
 
-// IncTopicDownvote increases topic downvote counts
-func IncTopicDownvote(name string) bool {
-	if v, ok := topicKV[name]; ok {
-		v.downvote = v.downvote + 1
-		return true
+// TopicListUpvote defines the topic array with upvote
+type TopicListUpvote []topic
+
+func (l TopicListUpvote) Len() int           { return len(l) }
+func (l TopicListUpvote) Swap(i, j int)      { l[i], l[j] = l[j], l[i] }
+func (l TopicListUpvote) Less(i, j int) bool { return l[i].upvote < l[j].upvote }
+
+// TopicListDownvote defines the topic array with downvote
+type TopicListDownvote []topic
+
+func (l TopicListDownvote) Len() int           { return len(l) }
+func (l TopicListDownvote) Swap(i, j int)      { l[i], l[j] = l[j], l[i] }
+func (l TopicListDownvote) Less(i, j int) bool { return l[i].downvote < l[j].downvote }
+
+// GetTopicDescendUpvote gets topics with desceding upvote order
+func GetTopicDescendUpvote() TopicListUpvote {
+	// Transfer map to array
+	uvList := make(TopicListUpvote, len(topicKV))
+	// Variable default value is 0
+	var index int
+	for k, v := range topicKV {
+		uvList[index] = topic{name: k, upvote: v.upvote, downvote: v.downvote}
+		index++
 	}
-	return false
+
+	sort.Sort(sort.Reverse(TopicListUpvote(uvList)))
+	return uvList
 }
 
-// DecTopicUpvote decreases topic upvote counts
-func DecTopicUpvote(name string) bool {
-	if v, ok := topicKV[name]; ok {
-		if v.upvote != 0 {
-			v.upvote = v.upvote - 1
-		}
-		return true
+// GetTopicDescendDownvote gets topics with desceding downvote order
+func GetTopicDescendDownvote() TopicListDownvote {
+	// Transfer map to array
+	dvList := make(TopicListDownvote, len(topicKV))
+	// Variable default value is 0
+	var index int
+	for k, v := range topicKV {
+		dvList[index] = topic{name: k, upvote: v.upvote, downvote: v.downvote}
+		index++
 	}
-	return false
-}
 
-// DecTopicDownvote decreases topic downvote counts
-func DecTopicDownvote(name string) bool {
-	if v, ok := topicKV[name]; ok {
-		if v.downvote != 0 {
-			v.downvote = v.downvote - 1
-		}
-		return true
-	}
-	return false
+	sort.Sort(sort.Reverse(TopicListDownvote(dvList)))
+	return dvList
 }
--- backend/cache/data_test.go
+++ backend/cache/data_test.go
@@ -1,1 +1,137 @@
 package cache
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIsTopicExist(t *testing.T) {
+	exist := IsTopicExist("1")
+	assert.Equal(t, false, exist, "The topic should not exist")
+}
+
+func TestCreateTopic(t *testing.T) {
+	ok := CreateTopic("2")
+	assert.Equal(t, true, ok, "Create topic failed")
+
+	// Test repeat create topic
+	ok = CreateTopic("2")
+	assert.Equal(t, true, ok, "Create topic failed")
+
+	exist := IsTopicExist("2")
+	assert.Equal(t, true, exist, "The topic should exist")
+}
+
+func TestDeleteTopic(t *testing.T) {
+	ok := DeleteTopic("3")
+	assert.Equal(t, true, ok, "Delete topic success")
+
+	ok = DeleteTopic("3")
+	assert.Equal(t, true, ok, "Delete topic failed")
+
+	ok = CreateTopic("3")
+	assert.Equal(t, true, ok, "Create topic failed")
+
+	ok = DeleteTopic("3")
+	assert.Equal(t, true, ok, "Delete topic failed")
+
+	exist := IsTopicExist("3")
+	assert.Equal(t, false, exist, "The topic should not exist")
+}
+
+func TestGetTopicUpvote(t *testing.T) {
+	vote := GetTopicUpvote("4")
+	assert.EqualValues(t, 0, vote, "The upvote should be zero")
+
+	ok := CreateTopic("4")
+	assert.Equal(t, true, ok, "Create topic failed")
+
+	ok = SetTopicUpvote("4", 1)
+	assert.Equal(t, true, ok, "Set topic upvote failed")
+
+	vote = GetTopicUpvote("4")
+	assert.EqualValues(t, 1, vote, "The upvote should be one")
+}
+
+func TestGetTopicDownvote(t *testing.T) {
+	vote := GetTopicDownvote("5")
+	assert.EqualValues(t, 0, vote, "The downvote should be zero")
+
+	ok := CreateTopic("5")
+	assert.Equal(t, true, ok, "Create topic failed")
+
+	ok = SetTopicDownvote("5", 1)
+	assert.Equal(t, true, ok, "Set topic downvote failed")
+
+	vote = GetTopicDownvote("5")
+	assert.EqualValues(t, 1, vote, "The downvote should be one")
+}
+
+func TestSetTopicUpvote(t *testing.T) {
+	ok := SetTopicUpvote("6", 100)
+	assert.Equal(t, false, ok, "Set topic upvote should failed")
+
+	ok = CreateTopic("6")
+	assert.Equal(t, true, ok, "Create topic failed")
+
+	ok = SetTopicUpvote("6", 100)
+	assert.Equal(t, true, ok, "Set topic upvote failed")
+
+	vote := GetTopicUpvote("6")
+	assert.EqualValues(t, 100, vote, "The upvote should be one-hundred")
+}
+
+func TestSetTopicDownvote(t *testing.T) {
+	ok := SetTopicDownvote("7", 100)
+	assert.Equal(t, false, ok, "Set topic downvote should failed")
+
+	ok = CreateTopic("7")
+	assert.Equal(t, true, ok, "Create topic failed")
+
+	ok = SetTopicDownvote("7", 100)
+	assert.Equal(t, true, ok, "Set topic downvote failed")
+
+	vote := GetTopicDownvote("7")
+	assert.EqualValues(t, 100, vote, "The downvote should be one-hundred")
+}
+
+func TestGetTopicDescendUpvote(t *testing.T) {
+	tests := make(TopicListUpvote, 5)
+	tests[0] = topic{"8-5", 9, 0}
+	tests[1] = topic{"8-2", 7, 0}
+	tests[2] = topic{"8-1", 5, 0}
+	tests[3] = topic{"8-3", 3, 0}
+	tests[4] = topic{"8-4", 1, 0}
+
+	for idx := range tests {
+		ok := CreateTopic(tests[idx].name)
+		assert.Equal(t, true, ok, "Create topic failed")
+
+		ok = SetTopicUpvote(tests[idx].name, tests[idx].upvote)
+		assert.Equal(t, true, ok, "Set topic upvote failed")
+	}
+
+	topicListUpvote := GetTopicDescendUpvote()
+	assert.ObjectsAreEqual(topicListUpvote, tests)
+}
+
+func TestGetTopicDescendDownvote(t *testing.T) {
+	tests := make(TopicListDownvote, 5)
+	tests[0] = topic{"9-5", 0, 9}
+	tests[1] = topic{"9-2", 0, 7}
+	tests[2] = topic{"9-1", 0, 5}
+	tests[3] = topic{"9-3", 0, 3}
+	tests[4] = topic{"9-4", 0, 1}
+
+	for idx := range tests {
+		ok := CreateTopic(tests[idx].name)
+		assert.Equal(t, true, ok, "Create topic failed")
+
+		ok = SetTopicDownvote(tests[idx].name, tests[idx].downvote)
+		assert.Equal(t, true, ok, "Set topic downvote failed")
+	}
+
+	topicListDownvote := GetTopicDescendDownvote()
+	assert.ObjectsAreEqual(topicListDownvote, tests)
+}
